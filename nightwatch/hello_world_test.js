6fa2aced30811725e6fbd346d450e35dfd4045ef
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-07-28 14:40:30 +0200

    fix End-to-End hello world test

--- nightwatch/hello_world_test.js
+++ nightwatch/hello_world_test.js
@@ -9,10 +9,10 @@ module.exports = {
             .click('input[type=submit')
             .waitForElementVisible('.results--main')
             .assert.visible('#r1-0')
-            .assert.visible('#r1-0 .result__a')
-            .assert.containsText('#r1-0 .result__a', 'Hello world — Wikipédia')
-            .click('#r1-0 .result__a')
+            .assert.visible('a[data-testid=result-title-a]')
+            .assert.textContains('a[data-testid=result-title-a]', 'Hello world — Wikipédia')
+            .click('a[data-testid=result-title-a]')
             .assert.visible('#firstHeading')
-            .assert.containsText('#firstHeading', 'Hello world')
+            .assert.textContains('#firstHeading', 'Hello world')
     }
 }
